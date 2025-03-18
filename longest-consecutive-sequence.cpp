55eabf020a2675267965af2ef68d479e53282107
Yeshal Khan <yeshalkhan13@gmail.com>
2025-03-18 23:23:43 +0500

    Update longest-consecutive-sequence.cpp

--- longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence.cpp
@@ -1,3 +1,6 @@
+// Time complexity: O(N)
+// Alternative approach is to sort the array and count length of each sequence and return the max, then complexity: O(NlogN)
+
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -7,16 +10,17 @@ public:
         unordered_set<int> s;
         for (int n : nums)
             s.insert(n);
-        int max = 0;
-        int count = 0, smallest;
+        int max = 0, count = 0;
         for (int x : s)
         {
-            if (s.count(x - 1) == 0)
+            if (s.count(x - 1) == 0)    // check if current element is the smallest in its sequence i.e., x-1 doesn't exist
             {
                 count = 1;
-                smallest = x;
-                while (s.count(++smallest))
+                while (s.count(++x))
+                {
                     count++;
+                    s.erase(x);    // remove element to avoid recomputation
+                }
             }
             if (count > max)
                 max = count;
@@ -24,3 +28,4 @@ public:
         return max;
     }
 };
+
