c52051acc7bb22bcfb55203233b78e334f74073c
Yeshal Khan <yeshalkhan13@gmail.com>
2025-03-19 20:54:44 +0500

    Create number-of-palindromic-substrings.cpp

---
+++ number-of-palindromic-substrings.cpp
@@ -0,0 +1,26 @@
+// Approach 1: Generate substrings and check if they're palindromic, Complexity: O(N^3)
+
+class Solution1 {
+public:
+    bool isPalindrome(string s)
+    {
+        if (s.size() == 1)
+            return true;
+        int mid =  s.size() / 2;
+        for (int i = 0, j = s.size() - 1; i < mid; i++, j--)
+        {
+            if (s[i] != s[j])
+                return false;
+        }
+        return true;
+    }
+    int countSubstrings(string s) {
+        int count = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            for (int j = i; j < s.size(); j++)
+                count += isPalindrome(s.substr(i, (j - i + 1)));
+        }
+        return count;
+    }
+};
